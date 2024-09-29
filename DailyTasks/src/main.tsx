6927df4f98b13678227eaa152d9799792a24fb82
quartazz <emptymailacc@proton.me>
2024-09-29 13:23:59 +0300

    вроде фикс таилвинда

--- DailyTasks/src/main.tsx
+++ DailyTasks/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './App.css'
-
+import './styles/tailwindcss.css'
 import './App.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
--- DailyTasks/tailwind.config.js
+++ DailyTasks/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,ts,vue}', './index.html'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './src/index.html'], // Убедитесь, что путь к index.html верный
 	theme: {
 		extend: {},
 	},
