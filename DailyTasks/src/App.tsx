8ce7b5e6b429aa9556ba61113a9bd35369394908
kawadaa <hayxper@gmail.com>
2024-09-30 00:39:02 +0800

    asdasd

--- DailyTasks/src/App.tsx
+++ DailyTasks/src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, ThemeProvider, createTheme, alpha } from '@mui/material'
+import list from '/src/img/list.svg';
 
 const theme = createTheme({
 	palette: {
@@ -12,6 +13,13 @@ const theme = createTheme({
 				root: {
 					// width: '100%',
 					// borderRadius: 0,
+
+					backgroundColor: '#141824',
+					color: '#AAAAAA',
+					width: '24rem',
+					borderRadius: '18px',
+					display: 'flex',
+					padding: '0.65rem'
 				},
 			},
 		},
@@ -21,28 +29,42 @@ const theme = createTheme({
 function App() {
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				<Button
-					draggable='false'
-					color='primary'
-					style={{ textTransform: 'none' }}
-				>
-					Material UI
-				</Button>
-			</ThemeProvider>
-			<div className='border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
-				<div className='animate-pulse flex space-x-4'>
-					<div className='rounded-full bg-slate-700 h-10 w-10'></div>
-					<div className='flex-1 space-y-6 py-1'>
-						<div className='h-2 bg-slate-700 rounded'></div>
-						<div className='space-y-3'>
-							<div className='grid grid-cols-3 gap-4'>
-								<div className='h-2 bg-slate-700 rounded col-span-2'></div>
-								<div className='h-2 bg-slate-700 rounded col-span-1'></div>
-							</div>
-							<div className='h-2 bg-slate-700 rounded'></div>
+			<div className='p-8'>
+				<div className="justify-between flex">
+					<ThemeProvider theme={theme}>
+						<div className="gap-3 flex">
+							<Button
+							draggable='false'
+							color='primary'
+							className='items-center'
+							style={{ textTransform: 'none' }}
+							>
+							Открыть списком
+							</Button>
+
+							<Button
+							draggable='false'
+							color='primary'
+							className='items-center'
+							style={{ textTransform: 'none' }}
+							>
+							Новая задача
+							</Button>
 						</div>
-					</div>
+
+						<div className="">
+						<Button
+							draggable='false'
+							color='primary'
+							className='items-center'
+							style={{ textTransform: 'none' }}
+							>
+							Профиль
+							</Button>
+						</div>
+					</ThemeProvider>
+
+
 				</div>
 			</div>
 		</>
